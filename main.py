29cfdeb6c59d72d10d617257fbf32acd74a65789
Cole Coleman <91431059+AppsByCole@users.noreply.github.com>
2023-02-06 16:28:45 -0500

    Functionality Rework
    After realizing that certain lines will not exist in a "diff", I modified my implementation to add new lines at the end of the file, rather than replacing the original lines.
    
    Also added VTX control options.

--- main.py
+++ main.py
@@ -4,84 +4,69 @@ video_systems = ["Avatar", "DJI O3", "DJI", "Analog"]
 receivers = ["FrSky", "Crossfire", "Ghost", "ELRS", "DJI"]
 
 
-def new_video_system_lines(line, video_system, file_name):
+def new_name_line(video_system, receiver, file_name):
+    prefix = file_name.split(" ")[0]
+    return f"set name = {prefix} {video_system} {receiver}"
+
+
+def new_video_system_lines(video_system, file_name):
+    new_lines = []
     if video_system == "Avatar":
-        if line.startswith("set osd_displayport_device = "):
-            return "set osd_displayport_device = MSP"
-        elif line.startswith("set displayport_msp_serial = "):
-            if file_name.startswith("Cinewhoop" or "Skylite" or "Moongoat"):
-                return "set displayport_msp_serial = 2"
-            return "set displayport_msp_serial = 1"
+        new_lines.append("set osd_displayport_device = MSP")
+        if file_name.startswith("Cinewhoop" or "Skylite" or "Moongoat"):
+            new_lines.append("set displayport_msp_serial = 2")
+        else:
+            new_lines.append("set displayport_msp_serial = 1")
     elif video_system == "DJI O3":
-        if line.startswith("set osd_displayport_device = "):
-            return "set osd_displayport_device = MSP"
-        elif line.startswith("set displayport_msp_serial = "):
-            return "set displayport_msp_serial = 1"
-        pass
+        new_lines.append("set osd_displayport_device = MSP")
+        if file_name.startswith("Cinewhoop" or "Skylite" or "Moongoat"):
+            new_lines.append("set displayport_msp_serial = 2")
+        else:
+            new_lines.append("set displayport_msp_serial = 1")
     elif video_system == "DJI":
-        if line.startswith("set osd_displayport_device = "):
-            return "set osd_displayport_device = AUTO"
+        new_lines.append("set osd_displayport_device = AUTO")
     elif video_system == "Analog":
-        if line.startswith("set osd_displayport_device = "):
-            return "set osd_displayport_device = AUTO"
-    return line
+        new_lines.append("set osd_displayport_device = AUTO")
+        if file_name.startswith("Cinewhoop" or "Skylite" or "Moongoat"):
+            new_lines.append("serial 2 8192 115200 57600 0 115200")
+        else:
+            new_lines.append("serial 2 8192 115200 57600 0 115200")
+    return "\n".join(new_lines)
 
 
-def new_receiver_lines(line, receiver):
+def new_receiver_lines(receiver):
+    new_lines = []
     if receiver == "FrSky":
-        if line.startswith("set serialrx_provider = "):
-            return "set serialrx_provider = SBUS"
-        elif line.startswith("map "):
-            return "map AETR1234"
+        new_lines.append("set serialrx_provider = SBUS")
+        new_lines.append("map AETR1234")
     elif receiver == "Crossfire":
-        if line.startswith("set serialrx_provider = "):
-            return "set serialrx_provider = CRSF"
-        elif line.startswith("map "):
-            return "map TAER1234"
+        new_lines.append("set serialrx_provider = CRSF")
+        new_lines.append("map TAER1234")
     elif receiver == "Ghost":
-        if line.startswith("set serialrx_provider = "):
-            return "set serialrx_provider = GHST"
-        elif line.startswith("map "):
-            return "map AETR1234"
+        new_lines.append("set serialrx_provider = GHST")
+        new_lines.append("map AETR1234")
     elif receiver == "ELRS":
-        if line.startswith("set serialrx_provider = "):
-            return "set serialrx_provider = CRSF"
-        elif line.startswith("map "):
-            return "map TAER1234"
+        new_lines.append("set serialrx_provider = CRSF")
+        new_lines.append("map TAER1234")
     elif receiver == "DJI":
-        if line.startswith("set serialrx_provider = "):
-            return "set serialrx_provider = SBUS"
-        elif line.startswith("map "):
-            return "map AETR1234"
-    return line
+        new_lines.append("set serialrx_provider = SBUS")
+        new_lines.append("map AETR1234")
+    return "\n".join(new_lines)
 
 
-def new_name_line(line, video_system, receiver, file_name):
-    if line.startswith("set name = "):
-        prefix = file_name.split(" ")[0]
-        return f"set name = {prefix} {video_system} {receiver}"
-    return line
-
-
-def new_aux_lines(line, video_system, receiver):
+def new_aux_lines(video_system, receiver):
+    new_lines = []
     if video_system == "DJI O3" and receiver == "DJI":
-        if line.startswith("aux 0"):
-            return "aux 0 0 3 1700 2100 0 0"
-        elif line.startswith("aux 1"):
-            return "aux 1 1 0 900 1650 0 0"
-        elif line.startswith("aux 2"):
-            return "aux 2 13 2 1700 2100 0 0"
-        elif line.startswith("aux 3"):
-            return "aux 3 35 1 1700 2100 0 0"
-    if line.startswith("aux 0"):
-        return "aux 0 0 0 1700 2100 0 0"
-    elif line.startswith("aux 1"):
-        return "aux 1 1 1 1700 2100 0 0"
-    elif line.startswith("aux 2"):
-        return "aux 2 13 3 1700 2100 0 0"
-    elif line.startswith("aux 3"):
-        return "aux 3 35 2 1700 2100 0 0"
-    return line
+        new_lines.append("aux 0 0 3 1700 2100 0 0")
+        new_lines.append("aux 1 1 0 900 1650 0 0")
+        new_lines.append("aux 2 13 2 1700 2100 0 0")
+        new_lines.append("aux 3 35 1 1700 2100 0 0")
+    else:
+        new_lines.append("aux 0 0 0 1700 2100 0 0")
+        new_lines.append("aux 1 1 1 1700 2100 0 0")
+        new_lines.append("aux 2 13 3 1700 2100 0 0")
+        new_lines.append("aux 3 35 2 1700 2100 0 0")
+    return "\n".join(new_lines)
 
 
 def new_file_name(file_name, video_system, receiver):
@@ -93,12 +78,15 @@ def new_file_content(file_contents, video_system, receiver, file_name):
     lines = file_contents.split("\n")
     modified_lines = []
     for line in lines:
-        modified_line = new_video_system_lines(line, video_system, file_name)
-        modified_line = new_receiver_lines(modified_line, receiver)
-        modified_line = new_aux_lines(line, video_system, receiver)
-        modified_line = new_name_line(
-            modified_line, video_system, receiver, file_name)
-        modified_lines.append(modified_line)
+        if not line.startswith("save"):
+            modified_lines.append(line)
+
+        if line.startswith("save"):
+            modified_lines.append(new_name_line(video_system, receiver, file_name))
+            modified_lines.append(new_video_system_lines(video_system, file_name))
+            modified_lines.append(new_receiver_lines(receiver))
+            modified_lines.append(new_aux_lines(video_system, receiver))
+            modified_lines.append(line)
     return "\n".join(modified_lines)
 
 
